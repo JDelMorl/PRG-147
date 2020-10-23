734e7ef697dab63817fe5627731404baab6cc99a
JDelMorl <54329605+JDelMorl@users.noreply.github.com>
2020-10-23 18:49:05 -0500

    Add files via upload

---
+++ numba_2.js
@@ -0,0 +1,11 @@
+function videoGames(title, protagonists) {
+    this.title = title;
+    this.protagonists = protagonists;
+    
+}   
+
+var games = [
+    new videoGames("Yakuza 0", "Kiryu Kazama, Goro Majima"),
+    new videoGames("Yakuza 4", "Kiryu Kazama, Taiga Saejima, Shun Akiyama, Masayoshi Tanimura")
+]
+console.log(games)
